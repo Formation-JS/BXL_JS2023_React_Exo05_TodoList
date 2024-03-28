213aa56ef12fe25d8af439bcb995e001d310d6c1
Aurélien <aurelien.strimelle@bstorm.be>
2024-03-28 11:25:49 +0100

    Handle new task 📝

--- src/containers/todo-list/todo-list.jsx
+++ src/containers/todo-list/todo-list.jsx
@@ -1,15 +1,27 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import TaskForm from '../../components/task-form/task-form.jsx';
 
 const TodoList = () => {
-     const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState([]);
+
+    const handleNewTask = (data) => {
+
+        const task = {
+            ...data,
+            id: uuidv4(),
+            isCompleted: false 
+        }
+
+        setTasks(tasks => [...tasks, task]);
+    }
 
     return (
         <>
             <h1>Correction</h1>
-            
+
             <h2>Ajouter une tache</h2>
-            <TaskForm />
+            <TaskForm onTaskSubmit={handleNewTask} />
         </>
     );
 };
